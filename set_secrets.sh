8901f9d0a1b60ebbc37dcdcce531357da5038c7b
Sivukhin Nikita <sivukhin.nikita@yandex.ru>
2019-05-19 03:18:09 +0500

    Fix path in set_secrets.sh script

--- set_secrets.sh
+++ set_secrets.sh
@@ -6,6 +6,6 @@ read token
 echo "Enter mailgun password: "
 read password
 
-echo $password > ./backend/EmailSenderDaemon/secret_password.config
-echo $token > ./backend/CleanCityBot/secret_token.config
-echo "false" > ./backend/CleanCityBot/secret_use_proxy.config
+echo $password > ./backend/Noname15/EmailSenderDaemon/secret_password.config
+echo $token > ./backend/Noname15/CleanCityBot/secret_token.config
+echo "false" > ./backend/Noname15/CleanCityBot/secret_use_proxy.config
