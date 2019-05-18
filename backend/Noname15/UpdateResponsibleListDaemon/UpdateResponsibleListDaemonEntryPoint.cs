d537135cce0c3d8bfbc8fd16ea25daeb51a25985
Sivukhin Nikita <sivukhin.nikita@yandex.ru>
2019-05-19 04:29:22 +0500

    Add one more microservice...why i doing this...?

---
+++ backend/Noname15/UpdateResponsibleListDaemon/UpdateResponsibleListDaemonEntryPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using CleanCityCore;
+
+namespace UpdateResponsibleListDaemon
+{
+    class UpdateResponsibleListDaemonEntryPoint
+    {
+        static void Main(string[] args)
+        {
+            var responsibleRepository = new ResponsibleRepository();
+            var responsibleFounder = new ResponsibleFounder(responsibleRepository);
+            Console.Out.WriteLine("Email sender started!");
+            while (true)
+            {
+                foreach (var responsible in responsibleFounder.GetAllResponsibles())
+                {
+                    responsibleRepository.AddResponsible(responsible);
+                }
+
+                Thread.Sleep(TimeSpan.FromMinutes(10));
+            }
+        }
+    }
+}
