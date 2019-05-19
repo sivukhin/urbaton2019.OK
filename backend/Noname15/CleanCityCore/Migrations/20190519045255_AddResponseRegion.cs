d5edcb1e6036169a612d964635d494cc695678ef
Sivukhin Nikita <sivukhin.nikita@yandex.ru>
2019-05-19 09:53:00 +0500

    Add migration for PostgreSQL

---
+++ backend/Noname15/CleanCityCore/Migrations/20190519045255_AddResponseRegion.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanCityCore.Migrations
+{
+    public partial class AddResponseRegion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ResponseRegion",
+                table: "ResponsibleSql",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResponseRegion",
+                table: "ResponsibleSql");
+        }
+    }
+}
